588825c8d262a857d166a5d71492a466bbe90c6a
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2024-11-24 11:02:00 +0200

    Modified the API start command

--- run.py
+++ run.py
@@ -1,9 +1,16 @@
+import sys
 import uvicorn
+
+args = sys.argv
+
+host = "127.0.0.1"
+if len(args) > 1:
+    host = args[1]
 
 if __name__ == "__main__":
     uvicorn.run(
         "summative.API.prediction:app",
-        host="127.0.0.1",
+        host=host,
         port=8000,
         reload=True
     )
